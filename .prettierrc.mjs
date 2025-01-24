ef0b19d41d73f4de71690d7e75cab96c5561f8c4
liangxiaojun <fatalxiao@163.com>
2025-01-24 11:06:49 +0800

    init nextjs

---
+++ .prettierrc.mjs
@@ -0,0 +1,23 @@
+const prettierrc = {
+    tabWidth: 4,
+    pluginSearchDirs: false,
+    plugins: [
+        'prettier-plugin-organize-imports',
+        'prettier-plugin-packagejson',
+    ],
+    organizeImportsSkipDestructiveCodeActions: true,
+    printWidth: 80,
+    proseWrap: 'never',
+    singleQuote: true,
+    trailingComma: 'all',
+    overrides: [
+        {
+            files: '*.md',
+            options: {
+                proseWrap: 'preserve',
+            },
+        },
+    ],
+};
+
+export default prettierrc;
